f334aaa25f31330fe69badb717049de393df4935
cuiqiusheng <363092524@qq.com>
2022-03-29 17:13:08 +0800

    feat: UI complete

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
-function Button() {
+import './style.scss'
+
+interface LayoutProps {
+    children?: React.ReactNode
+    className?: string
+    disabled?: boolean
+    onClick?: React.MouseEventHandler<HTMLElement>
+}
+
+function Button(props: LayoutProps) {
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>) => {
+        const { onClick, disabled } = props;
+        if (disabled) {
+            e.preventDefault();
+            return;
+        }
+        (onClick as React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>)?.(e);
+    };
+
     return (
-        <></>
+        <button className="button" onClick={handleClick}>
+            {props.children}
+        </button>
     )
 }
 
--- src/page/Invitation/Invitation.tsx
+++ src/page/Invitation/Invitation.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
+import Button from '../../components/Button'
+
+import './style.scss'
 
 function Invitation() {
     return (
         <Layout>
-            <div>content</div>
+            <div className="content">
+                <div>
+                    <div className="slogan">A better way <br/> to enjoy every day.</div>
+                    <div className="subheading">Be the first to know when we launch.</div>
+                    <div className="request-button"><Button>Request an invite</Button></div>
+                </div>
+            </div>
         </Layout>
     )
 }
